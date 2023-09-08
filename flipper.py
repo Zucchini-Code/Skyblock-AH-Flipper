1aa2c1909d7677fa3a8c4d807ca050fd2df282f9
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-08 23:23:09 +1000

    working sort of

--- flipper.py
+++ flipper.py
@@ -4,9 +4,16 @@ import pandas as pd
 import time
 
 while True:
-    auction_request = requests.get("https://api.hypixel.net/skyblock/auctions")
-    time.sleep(5)
-    auction_data = pd.DataFrame(auction_request.json()["auctions"])
+    auction_request = requests.get("https://api.hypixel.net/skyblock/auctions").json()
 
-    
-    
+    auction_pages = auction_request["totalPages"]
+    auction_number = auction_request["totalAuctions"]
+    auction_data = auction_request["auctions"]
+
+    for pages in range(1, auction_pages):
+        auction_data += requests.get(f"https://api.hypixel.net/skyblock/auctions?page={pages}").json()["auctions"]
+        print(f"Loading Pages: {pages}/{auction_pages}")
+    auction_data = pd.DataFrame(auction_data)
+
+    print(auction_data)
+    time.sleep(30)
