10b1ca341c62cfdc1191245054065cfc63c782fb
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-08 23:36:58 +1000

    removed non bin

--- flipper.py
+++ flipper.py
@@ -2,6 +2,7 @@ import pygame
 import requests
 import pandas as pd
 import time
+import asyncio
 
 while True:
     auction_request = requests.get("https://api.hypixel.net/skyblock/auctions").json()
@@ -9,11 +10,11 @@ while True:
     auction_pages = auction_request["totalPages"]
     auction_number = auction_request["totalAuctions"]
     auction_data = auction_request["auctions"]
-
+    
     for pages in range(1, auction_pages):
         auction_data += requests.get(f"https://api.hypixel.net/skyblock/auctions?page={pages}").json()["auctions"]
-        print(f"Loading Pages: {pages}/{auction_pages}")
+        print(f"Loading Pages: {pages}/{auction_pages - 1}")
     auction_data = pd.DataFrame(auction_data)
-
+    auction_data = auction_data.loc[auction_data.bin, :]
     print(auction_data)
     time.sleep(30)
