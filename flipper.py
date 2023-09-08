25b5cef9b0707b91fdc6430fe2b226facec54119
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-08 22:53:24 +1000

    changed anmes

--- flipper.py
+++ flipper.py
@@ -4,9 +4,9 @@ import pandas as pd
 import time
 
 while True:
-    auction_data = requests.get("https://api.hypixel.net/skyblock/auctions")
+    auction_request = requests.get("https://api.hypixel.net/skyblock/auctions")
     time.sleep(5)
-    print(pd.DataFrame(auction_data.json()["auctions"]))
+    auction_data = pd.DataFrame(auction_request.json()["auctions"])
 
     
     
