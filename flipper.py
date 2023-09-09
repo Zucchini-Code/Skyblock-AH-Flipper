90e65038f991accc5c0f96f90dfec38888fd2cd8
alexgmex <mcdino3812@gmail.com>
2023-09-09 16:41:00 +1000

    waiting for stackexchange

--- flipper.py
+++ flipper.py
@@ -9,16 +9,17 @@ import time
 
 # Get individual page asynchronously using AIOHTTP
 async def get_page(page_number):
+    print("Requesting Page: " + str(page_number + 1))
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://api.hypixel.net/skyblock/auctions?page={page_number}") as response:
             page = await response.json()
-            print("Got Page " + str(page["page"] + 1))
+            print("Got Page: " + str(page["page"] + 1))
             return page["auctions"]
 
 
-# Use asyncio to "gather" all pages asynchrononously and return list
+# Use asyncio to "gather" all pages asynchrononously and return as list
 async def get_all_pages(total_pages):
-    auction_data = await asyncio.gather(*(get_page(page_number) for page_number in range(total_pages - 1)))
+    auction_data = await asyncio.gather(*(get_page(page_number) for page_number in range(total_pages)))
     return auction_data
 
 
@@ -29,10 +30,10 @@ while True:
     print("Total Pages: " + str(total_pages))
     
     # Asynchronously grab all pages at once, and convert to pandas dataframe
-    auction_data = pd.DataFrame(asyncio.run(get_all_pages(total_pages)))
+    auction_data = asyncio.run(get_all_pages(total_pages))
 
     # Remove all auctions that aren't BIN auctions
     # auction_data = auction_data.loc[auction_data.bin, :]
 
-    print(auction_data)
+    # print(auction_data)
     time.sleep(15)
