30eb636c921385e3713dd81fdffd550e7e516ee5
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-10 02:33:29 +1000

    fixing tests

--- flipper.py
+++ flipper.py
@@ -9,7 +9,7 @@ import time
 async def get_page(session, url):
     async with session.get(url) as response:
         page = await response.json()
-        print("Got Page " + str(page['page']))
+        print("Got Page " + str(page['page'] + 1))
         return page['auctions']
 
 async def main():
@@ -19,28 +19,24 @@ async def main():
         tasks = []
         for page_number in range(total_pages):
             url = f"https://api.hypixel.net/skyblock/auctions?page={page_number}"
+            print("Requesting Page " + str(page_number + 1))
             tasks.append(asyncio.ensure_future(get_page(session, url)))
 
         # Then gather and return all responses as a list
         return await asyncio.gather(*tasks)
 
-# Infinite loop of grabbing auctions. Repeats every 15 seconds.
 
 start = time.time()
 
-# Get the current number of total pages
-# total_pages = requests.get("https://api.hypixel.net/skyblock/auctions").json()["totalPages"]
 total_pages = 60
 print("Total Pages: " + str(total_pages))
-
 asyncio.run(main())
-
-# Remove all auctions that aren't BIN auctions
-# auction_data = auction_data.loc[auction_data.bin, :]
-
-# print(auction_data)
 
 end = time.time()
 elapsed = end - start
 print("Operation took " + str(elapsed) + " seconds")
-time.sleep(15)
+
+
+# total_pages = requests.get("https://api.hypixel.net/skyblock/auctions").json()["totalPages"]
+# Remove all auctions that aren't BIN auctions
+# auction_data = auction_data.loc[auction_data.bin, :]
