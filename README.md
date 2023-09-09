b88b0a62ba3b71f1d7ce295fedc0b5fdaae5e9eb
alexgmex <mcdino3812@gmail.com>
2023-09-09 14:31:28 +1000

    sort of working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Skyblock_AH_Flipper
 ## Get prof by flipping items on Skyblock AH
+
+pip install pygame requests aiohttp numpy pandas
--- flipper.py
+++ flipper.py
@@ -6,28 +6,33 @@ import pandas as pd
 import asyncio
 import time
 
-# Define asynchronous functions to collect a large number of pages from API data in parallel
-# Get individual pages asynchronously 
-async def get_page(page):
+
+# Get individual page asynchronously using AIOHTTP
+async def get_page(page_number):
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.hypixel.net/skyblock/auctions?page={page}") as response:
-            checkPage = await response.json()
-            print(checkPage["page"])
-            return checkPage["page"]
+        async with session.get(f"https://api.hypixel.net/skyblock/auctions?page={page_number}") as response:
+            page = await response.json()
+            print("Got Page " + str(page["page"] + 1))
+            return page["auctions"]
 
+
+# Use asyncio to "gather" all pages asynchrononously and return list
 async def get_all_pages(total_pages):
-    pages = await asyncio.gather(*(get_page(page) for page in range(total_pages)))
-    return pages
+    auction_data = await asyncio.gather(*(get_page(page_number) for page_number in range(total_pages)))
+    return auction_data
 
+
+# Infinite loop of grabbing auctions. Repeats every 15 seconds.
 while True:
+    # Get the current number of total pages
     total_pages = requests.get("https://api.hypixel.net/skyblock/auctions").json()["totalPages"]
-
-    total_pages = auction_request
+    print("Total Pages: " + str(total_pages))
     
-    for pages in range(1, auction_pages):
-        auction_data += requests.get(f"https://api.hypixel.net/skyblock/auctions?page={pages}").json()["auctions"]
-        print(f"Loading Pages: {pages}/{auction_pages - 1}")
-    auction_data = pd.DataFrame(auction_data)
+    # Asynchronously grab all pages at once, and convert to pandas dataframe
+    auction_data = pd.DataFrame(asyncio.run(get_all_pages(total_pages)))
+
+    # Remove all auctions that aren't BIN auctions
     auction_data = auction_data.loc[auction_data.bin, :]
+
     print(auction_data)
-    time.sleep(30)
+    time.sleep(15)
