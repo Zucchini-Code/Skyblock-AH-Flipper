2782caed0564543968040e93b93e87f6d9d21c5b
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-11 01:11:01 +1000

    added comments

--- flipper.py
+++ flipper.py
@@ -6,12 +6,15 @@ import datetime
 import aiohttp
 import asyncio
 
+
+# Asynchronous function for getting a single page
 async def get_page(session, url):
     async with session.get(url) as response:
         page = await response.json()
         print("Got Page " + str(page['page'] + 1))
         return page['auctions']
 
+# Asynchronously get all pages with aiohttp
 async def main(total_pages):
     async with aiohttp.ClientSession() as session:
 
@@ -30,19 +33,23 @@ async def main(total_pages):
 # Start timing operation
 start = time.time()
 
-#this sends for the total number of pages in the auction list and runs the asyncronous stuff
+# Initial request to get the time last updated and the total page count
 initial_request = requests.get("https://api.hypixel.net/skyblock/auctions").json()
 total_pages = initial_request["totalPages"]
 update_time = initial_request["lastUpdated"]
-auction_data = asyncio.run(main(total_pages)) #returns a list of list of dicts
+
+# Asynchronously call all pages, then flatten list
+auction_data = asyncio.run(main(total_pages))
 auction_data = [item for sublist in auction_data for item in sublist]
 
-#the below will have to change to get the data into a df
+# Make flattened list into dataframe and remove all non-bin
 auction_data = pd.DataFrame(auction_data)
 auction_data = auction_data.loc[auction_data.bin, :]
 
+# Print dataframe
 print(auction_data)
 
+# End timing operation
 end = time.time()
 elapsed = end - start
 print("Operation took " + str(elapsed) + " seconds")
