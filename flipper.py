067fe81dc124862baaa274511031d8a398b447f1
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-10 14:21:08 +1000

    timesteps

--- flipper.py
+++ flipper.py
@@ -2,6 +2,7 @@ import pygame
 import requests
 import pandas as pd
 import time
+import datetime
 
 # Start timing operation
 start = time.time()
@@ -12,8 +13,10 @@ auction_data = initial_request["auctions"]
 
 print("Total Pages: " + str(total_pages))
 for page_number in range(1,total_pages):
-    print(f"Getting Page {page_number + 1}/{total_pages}")
-    auction_data += requests.get(f"https://api.hypixel.net/skyblock/auctions?page={page_number}").json()["auctions"]
+    response = requests.get(f"https://api.hypixel.net/skyblock/auctions?page={page_number}").json()
+    time_updated = datetime.datetime.fromtimestamp(response["lastUpdated"]/1000)
+    auction_data += response["auctions"]
+    print(f"Got Page {page_number + 1}/{total_pages}, Last Updated {time_updated}")
 
 auction_data = pd.DataFrame(auction_data)
 auction_data = auction_data.loc[auction_data.bin, :]
