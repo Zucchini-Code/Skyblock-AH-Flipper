9f0f243b71fc3ed1aa0d47cead12c791a2b272f8
alex-gmex-7 <alex.j.gmex@gmail.com>
2023-09-12 13:50:42 +1000

    fixed position of waiting for update

--- flipper.py
+++ flipper.py
@@ -18,15 +18,16 @@ print("Async operation took " + str(round(end - start)) + " seconds.")
 # Infinite loop
 while True:
 
+    # Wait for update
+    f.wait_for_update()
+
     # Get new auctions and auctions ended in the last minute
+    print("Getting new auction data...")
     new_auctions = pd.DataFrame(requests.get("https://api.hypixel.net/skyblock/auctions?page=0").json()["auctions"])
     ended_auctions = pd.DataFrame(requests.get("https://api.hypixel.net/skyblock/auctions_ended").json()["auctions"])
 
-    # Print dataframe
+    # Print dataframe and save CSVs
     print(auction_data)
     auction_data.to_csv("CSVs/auction_data.csv")
     new_auctions.to_csv("CSVs/new_auctions.csv")
     ended_auctions.to_csv("CSVs/ended_auctions.csv")
-
-    # Then wait for update
-    f.wait_for_update()
